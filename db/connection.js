cd7959ff31fcbb748f00fa6c64e1f61d367ef02d
ritik008 <ritikthakur27rt@gmail.com>
2024-02-25 10:27:07 +0530

    changes:

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose')
 const connectDb = async () => {
     try {   
         mongoose.set('strictQuery', true);
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          })
+        await mongoose.connect(process.env.MONGO_URI)
           console.log('connected to mongodb')
     }catch(err) {
         console.log(err.message)
